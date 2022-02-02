49e64b15817ae42955233d00d8a287e3f2c06840
denis1059 <denivaldo1059@gmail.com>
2022-02-02 16:36:03 -0300

    adiciona home

---
+++ test.js
@@ -0,0 +1,22 @@
+const data = new Date();
+const diaSemana = data.getDay();
+let diaSemanaTexto;
+
+if (diaSemana === 0 ){
+   diaSemanaTexto = 'Domingo';
+
+}else if (diaSemana === 1 ){
+    diaSemanaTexto = 'Segunda';
+}else if (diaSemana === 2 ){
+    diaSemanaTexto = 'Terça';
+}else if (diaSemana === 3 ){
+    diaSemanaTexto = 'Quarta';
+}else if (diaSemana === 4 ){
+    diaSemanaTexto = 'Quinta';
+}else if (diaSemana === 5 ){
+    diaSemanaTexto = 'Sexta';
+}else if (diaSemana === 6 ){
+    diaSemanaTexto = 'Sabado';
+}
+
+console.log(diaSemanaTexto);
