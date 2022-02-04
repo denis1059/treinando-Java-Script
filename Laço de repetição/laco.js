2fe5bc67184b5963797b390c9e77487f592a8fec
denis1059 <denivaldo1059@gmail.com>
2022-02-04 08:39:08 -0300

    laço de repetição com array

--- Laço de repetição/laco.js
+++ Laço de repetição/laco.js
@@ -1,4 +1,22 @@
-for  (let i = 0; i <=5; i++){
-console.log(i);
-}
+// verifica se o numero é par
 
+/*for  (let i = 0; i <=5; i++){
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log( i, par);
+    }
+*/
+
+
+
+/*for  (let i = 0; i <=5; i++){
+console.log(`Linha ${i}`);
+}
+*/
+
+
+//                 0       1       2  
+const frutas = ['Maçã', 'Pera', 'Uva']
+for (let i = 0; i < frutas.length; i++){
+    console.log(`Indice ${i}`, frutas[i]);
+
+}
