3259a4b98205ae288695179fcde901bc7e90024c
denis1059 <denivaldo1059@gmail.com>
2022-02-05 10:49:43 -0300

    adiciona exercicio com for in

---
+++ exercicio com for in/index.js
@@ -0,0 +1,20 @@
+
+const pessoa = {
+    nome: 'Denis',
+    sobrenome: 'souza',
+    idade: '23'
+}
+
+for (let i in pessoa){
+    console.log(i, pessoa [i])
+}
+
+//iteração
+
+/*const frutas = ['Pera', 'Maçã', 'Uva'];
+
+for (let indice in frutas){
+    console.log(frutas[indice]);
+}
+*/
+
