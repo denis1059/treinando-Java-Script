012076f01c40315001eb3251a56fc3f84d04cbc2
denis1059 <denivaldo1059@gmail.com>
2022-02-19 14:59:27 -0300

    adiciona array-splite

---
+++ Array - splite/app.js
@@ -0,0 +1,14 @@
+// transforma string em uma array
+
+const nome = 'Denis Souza' ;
+const nomes = nome.split(' ');
+
+console.log(nomes);
+
+// transfroma array em string
+
+const nomes2 = [ 'Denis', 'Souza' ]
+const nome2 = nomes2.join(', ');
+
+console.log(nome2);
+
