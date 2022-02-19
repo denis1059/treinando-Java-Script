2f56c4d58674517e9568ce43b81006749c910934
denis1059 <denivaldo1059@gmail.com>
2022-02-19 14:48:40 -0300

    adiciona array-slice

---
+++ Array - Slice/app.js
@@ -0,0 +1,5 @@
+//                 0          1       2         3          4
+const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+const novo = nomes.slice(1,3); // pode se utilizar o -1 pra percorrer no array
+console.log(novo);
+
