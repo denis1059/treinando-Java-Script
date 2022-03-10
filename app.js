e87d63e780ad45b31dea5887dc9402a235addd9c
denis1059 <denivaldo1059@gmail.com>
2022-03-10 20:54:40 -0300

    Adiciona função Get e Setter 2

---
+++ app.js
@@ -0,0 +1,15 @@
+var one = 'Hello, ';
+var two = 'how are you?';
+var multiple = one + two;
+/*
+if(multiple.indexOf ('Hello, how are you?') == 1){
+
+    console.log('algo');
+}
+*/
+multiple.replace('carro');
+console.log(multiple);
+
+
+
+
