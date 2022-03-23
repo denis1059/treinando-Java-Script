c6a32d61253a157b9f7a89dece0fc32b1ca71b7a
denis1059 <denivaldo1059@gmail.com>
2022-03-22 21:16:48 -0300

    Atualiza validador de formulario

--- Validando um formulario/app.js
+++ Validando um formulario/app.js
@@ -14,19 +14,85 @@ class ValidaFormulario {
     handleSubmit(e) {
         e.preventDefault();
         const camposValidos = this.camposSaoValidos();
+        const senhasValidas = this.senhasSaoValidas();
+
+        if(camposValidos && senhasValidas){
+            alert('enviado');
+            this.formulario.submit();
+        }
     
+    }
+    senhasSaoValidas() {
+        let valid = true;
+
+          const senha = this.formulario.querySelector('.senha');
+          const repetirSenha = this.formulario.querySelector('.repetir-senha'); 
+          
+          if(senha.value != repetirSenha.value){
+              valid = false;
+              this.criaErro(senha, 'Campos senhas e repetir senha precisam ser iguais');
+              this.criaErro(repetirSenha, 'Campos senhas e repetir senha precisam ser iguais');
+          }
+
+          if(senha.value.length < 6 || senha.value.length > 12) {
+              valid = false;
+              this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres ')
+          }
+        return true;
+
     }
 
     camposSaoValidos(){
         let valid = true;
 
-        for(let campo of this.formulario.querySelectorAll('.validar')){
-          if(!campo.value){
-              this.criaErro( campo, 'Campo nao preenchido');
-              valid =  false;
-          }
+        for(let errorText of this.formulario.querySelectorAll('.error-text')){
+            errorText.remove();
         }
 
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            const label = campo.previousElementSibling.innerText;
+          if(!campo.value){
+              this.criaErro( campo, `Campo ${label} nao preenchido`);
+              valid =  false;
+          }
+
+          if(campo.classList.contains('cpf')) {
+              if(!this.validaCPF(campo)) valid = false ; 
+          }
+
+          if(campo.classList.contains('usuario')) {
+            if(!this.validaUsuario(campo)) valid = false ; 
+        }
+
+        }
+        return valid;
+    }
+
+    validaUsuario(campo){
+
+        const usuario = campo.value;
+        let valid = true;
+
+        if(usuario.length < 3 || usuario.length > 12) {
+            this.criaErro(campo, 'Usuario Precisa ter entre 3 e 12 caracteres.');
+            valid = false;
+        }
+
+        if(!usuario.match(/[a-zA-Z0-9]+/g)) {
+            this.criaErro(campo, 'Nome de usuario ´recisa conter apenas letras e ou numeros');
+            valid = false;
+        }
+        return valid;
+    }
+
+    validaCPF(campo){
+        const cpf = new ValidaCPF(campo.value);
+
+        if(!cpf.valida()){
+            this.criaErro(campo, 'CPF Invalido.');
+        return false;
+        }
+        return true;
     }
 
     criaErro(campo, msg){
@@ -37,4 +103,4 @@ class ValidaFormulario {
     }
 }
 
-const valida = new ValidaFormulario();
+const valida = new ValidaFormulario(); 
